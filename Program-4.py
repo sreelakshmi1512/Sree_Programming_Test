9085785c07eb095f1b1bcb667bebe3c9452d176b
Sree_Lakshmi <144984092+sreelakshmi1512@users.noreply.github.com>
2025-08-14 15:12:04 +0530

    Program-4.py

--- Program-4.py
+++ Program-4.py
@@ -1,1 +1,12 @@
+def multiple_count(numbers):
+    result = {}
+    for i in range(1, 10):     
+        result[i] = 0          
+    for num in numbers:
+        for i in range(1, 10):
+            if num % i == 0:  
+                result[i] = result[i] + 1:
+    return result
+numbers = [1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30]
+print("Output:", multiple_count(numbers))
 
