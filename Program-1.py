e310e77707ea052c86856a58c08ed5ac9dfd5eec
Sree_Lakshmi <144984092+sreelakshmi1512@users.noreply.github.com>
2025-08-14 14:56:25 +0530

    Program-1.py

--- Program-1.py
+++ Program-1.py
@@ -1,1 +1,18 @@
-
+class Calculator:
+    def __init__(self,a:float,b:float,operation:str):
+      self.a=a
+      self.b=b
+      self.operation=operation
+    def calculate(self):
+      if self.operation == "add":
+        return self.a + self.b
+      elif self.operation == "sub":
+        return self.a - self.b
+      elif self.operation == "mul":
+        return self.a * self.b
+      elif self.operation == "div":
+        return self.a / self.b if self.b != 0 else "Error: Division by zero"
+      else:
+        return "Invalid operation"
+calc = Calculator(10, 5, "div")  
+print("Result:", calc.calculate())
